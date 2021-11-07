dbfe0a7a8b69fefd8c62e4b7ab056c4d7b380aef
Jaffé Marques <jaffemarquessilva@gmail.com>
2021-11-07 08:52:55 -0300

    Chore: Adicionando e configurando Apollo e GraphQl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import "./index.css";
 import "@fontsource/dm-sans";
+
 import { AppProps } from "next/app";
 import { Chakra } from "systems/Core";
 
+import { ApolloProvider } from "@apollo/client";
+import { client } from "systems/Api";
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Chakra>
-      <Component {...pageProps} />
-    </Chakra>
+    <ApolloProvider client={client}>
+      <Chakra>
+        <Component {...pageProps} />
+      </Chakra>
+    </ApolloProvider>
   )
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,38 @@
 import { Layout } from "systems/Core/components/Layout";
-import { Button } from "@chakra-ui/react";
+import { gql, useQuery } from "@apollo/client";
+import { css } from "@emotion/react";
+import tw from "twin.macro";
+import { Spinner } from "@chakra-ui/spinner";
+
+const EVENTS_QUERY = gql`
+  query EventsQuery {
+    events {
+      id
+      title
+    }
+  }
+`;
 
 export default function Home() {
-  return <Layout title="Events App">
-    <div className="">
-      <Button colorScheme="primary" m={10}>
-        Click
-      </Button>
- 
-    </div>
-  </Layout>
+  const { loading, error, data } = useQuery(EVENTS_QUERY)
+
+  return (
+    <Layout title="Events App">
+      {loading ? <div css={styles.root}><Spinner /></div> : (
+        <div css={styles.root}>
+          {data?.events.map((event: any) => <div key={event.id}>{event.title}</div>)}
+        </div>
+      )}
+      
+    </Layout>
+  )
 }
 
+const styles = {
+  root: () => css`
+    ${tw`flex flex-col items-center justify-center width[100vw] height[100vh]`}
+  `
+}
 
 
 
